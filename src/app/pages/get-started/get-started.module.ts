4267657853af3ecaf8348a50aca4ae242731c2f8
Siva <gnusiva@gmail.com>
2021-02-01 21:29:28 +0530

    9 step component created

--- src/app/pages/get-started/get-started.module.ts
+++ src/app/pages/get-started/get-started.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GetStartedRoutingModule } from './get-started-routing.module';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
+import { Step4Component } from './step4/step4.component';
+import { Step5Component } from './step5/step5.component';
+import { Step6Component } from './step6/step6.component';
+import { Step7Component } from './step7/step7.component';
+import { Step8Component } from './step8/step8.component';
+import { Step9Component } from './step9/step9.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [Step1Component, Step2Component, Step3Component, Step4Component, Step5Component, Step6Component, Step7Component, Step8Component, Step9Component],
   imports: [
     CommonModule,
     GetStartedRoutingModule
