7bebabf90ac629ac6a711c280e05139f1cfa8a5b
Siva <sivam1@netapp.com>
2021-02-03 21:24:29 +0530

    backend done

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from "express"
-
+import { Request, Response } from "express";
+const db = require('./db');
+const cors = require('cors');
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json());
+app.use(cors());
+app.get('/getProductCategory', (req: Request, res: Response) => {
+  res.send(db.get('productCategories').value())
+});
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-})
+app.post('/createNewUser', (req: Request, res: Response) => {
+  console.log(req.body);
+  db.get('users').push(req.body).write();
+  res.send();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
