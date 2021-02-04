66fd984282d6f7720db124e7986d9c15aa31b76d
Siva <gnusiva@gmail.com>
2021-02-04 14:17:30 +0530

    all done

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then( m => m.HomeModule )},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginModule )},
   { path: 'get-started', loadChildren: () => import('./pages/get-started/get-started.module').then( m => m.GetStartedModule) }
 ];
 
