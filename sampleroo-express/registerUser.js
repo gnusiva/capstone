08608c8b3a182e0f9719d3f0d487e4fa4986db9e
Siva <sivam1@netapp.com>
2021-03-01 00:02:37 +0530

    user registration api intergration done

--- sampleroo-express/registerUser.js
+++ sampleroo-express/registerUser.js
@@ -2,11 +2,30 @@
 const registerUser = async (req, res) => {
     const db = req.db;
     const postData = req.body;
+    const {
+        email, 
+        password,
+        age,
+        userdetailsTerms,
+        firstName,
+        address,
+        field1,
+        province,
+        lastName,
+        unit,
+        postalCode,
+        country,
+        shippingTerms,
+        subCategoryId,
+        brandId,
+        productId
+      } = postData;
     console.log(postData);
     db.serialize( () => {
         const query = `INSERT INTO "main"."Users"("id","isSocial","loginProvider","socialId","email","password","isEmailVerified","age","firstName","lastName","address","unit","field1","province","postalCode","country","productsId","subCategoriesId","brandsId") 
-                                          VALUES (NULL,"false",NULL,NULL,                     NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);`;
-        res.send();
+                                          VALUES (NULL,"false",NULL,NULL,                     "${email}","${password}","false",     "${age}","${firstName}","${lastName}","${address}","${unit}","${field1}","${province}","${postalCode}","${country}","${productId}","${subCategoryId}","${brandId}");`;
+        db.run(query);
+        res.send({});
     });
 }
 
