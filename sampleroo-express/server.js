1d85c45de4c16b0a895d7fa4289b86534e28dd37
Siva <sivam1@netapp.com>
2021-02-28 21:03:48 +0530

    subCategory page integration done

--- sampleroo-express/server.js
+++ sampleroo-express/server.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 3000
 const attachDatabase = require('./dbProvider');
 const getProducts = require('./getProducts');
+const getSubCategory = require('./getSubCategory');
 app.use(express.static('html'));
 app.use(attachDatabase);
 app.get('/products/all', getProducts);
+app.get('/products/category/:productId', getSubCategory);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
