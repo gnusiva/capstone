9c71433859996f7651bd03c2a6f4f576952594ff
Siva <sivam1@netapp.com>
2021-03-01 01:41:45 +0530

    email verificaiton message on login page

--- sampleroo-express/logni.js
+++ sampleroo-express/logni.js
@@ -5,7 +5,8 @@ const login = async (req, res) => {
         const query = `SELECT * FROM "Users" WHERE email = "${req.body.email}" AND password = "${req.body.password}"`;
         console.log(query, req.body )
         db.all(query, (err, rows) => {
-            res.send(rows.map(item => item.email));
+            console.log(rows);
+            res.send(rows.map(item => item.isEmailVerified));
         });
     });
 }
